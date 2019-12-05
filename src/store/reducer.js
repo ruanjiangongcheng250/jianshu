da404e9db9db6cdf8ab55527342e95b890e6068a
zackon <1032094832@qq.com>
2019-12-05 20:22:36 +0800

    111

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -3,12 +3,14 @@ import { reducer as headerReducer } from '../common/header/store';
 import { reducer as homeReducer} from '../pages/home/store';
 import { reducer as detailReducer } from '../pages/detail/store';
 import { reducer as loginReducer } from '../pages/login/store';
+import { reducer as SearchReducer } from '../pages/search/store';
 
 const reducer =  combineReducers({
     header: headerReducer,
     home: homeReducer,
     detail: detailReducer,
-    login: loginReducer
+    login: loginReducer,
+    search: SearchReducer
 })
 
 export default reducer;
